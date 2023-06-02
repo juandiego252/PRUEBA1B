1e71f07c6cbd3263552ce778af02188a6b31888d
Juan Diego <juandiegojd252@gmail.com>
2023-06-02 12:21:59 -0500

    Clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.io.StringReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // Instancias de la clase VideoJuego
+        VideoJuego grand_thef_auto = new VideoJuego(2013,"GTA5","Mundo Abierto",18,"Violencia");
+        VideoJuego resident_evil = new VideoJuego(2023,"Resident Evil 4 Remake","Terror", 18,"Violencia");
+        VideoJuego minecraft = new VideoJuego(2013,"Minecraft","Construccion",12,"Supervivencia");
+        VideoJuego valorant = new VideoJuego(2019,"Valorant","Shooter",12,"Esport");
+        VideoJuego fifa = new VideoJuego(2022,"FIFA 23","Competencia",12,"Deportes");
+
+        // Llamamos a los objetos y su metodo imprimir datos
+        grand_thef_auto.imprimirDatos();
+        resident_evil.imprimirDatos();
+        minecraft.imprimirDatos();
+        valorant.imprimirDatos();
+        fifa.imprimirDatos();
+
+    }
 }
--- src/VideoJuego.java
+++ src/VideoJuego.java
@@ -1,2 +1,74 @@
-package PACKAGE_NAME;public class VideoJuego {
+import javax.sound.midi.Soundbank;
+
+public class VideoJuego {
+
+    // Creacion de los Atributos de la clase Video Juegos
+    int anio;
+    String nombre;
+    String tipo;
+    int edad;
+    String categoria;
+
+    // Constructor
+    public VideoJuego(int anio, String nombre, String tipo, int edad, String categoria) {
+        this.anio = anio;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.edad = edad;
+        this.categoria = categoria;
+    }
+
+    // Creacion de los setters y getters de los atributos
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+
+    // Metodo para imprimir datos de los de videoJuegos
+    public void imprimirDatos(){
+
+        System.out.println("------------------------------------------");
+        System.out.println("Nombre:" + getNombre());
+        System.out.println("Año de Lanzamiento:" + getAnio());
+        System.out.println("Tipo de Video Juego:" + getTipo());
+        System.out.println("Categoria:" + getCategoria());
+        System.out.println("Edad permitida: +" + getEdad());
+        System.out.println("-------------------------------------------");
+    }
 }
